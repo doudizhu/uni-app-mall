dd773b94a9651319eafbe3f04e017642bd8b0e23
lipeng <lipeng@daokoudai.com>
2019-11-26 15:07:57 +0800

    5.uni-app实战商城项目-自定义顶部导航栏

--- pages/tabBar/home/index.vue
+++ pages/tabBar/home/index.vue
@@ -1,8 +1,16 @@
 <template>
+	<!-- 自定义顶部导航栏 -->
+	<pageHeader />
 </template>
 
 <script>
+	import pageHeader from './pageHeader.vue'
+	export default{
+		components:{
+			pageHeader,
+		}
+	}
 </script>
 
-<style>
+<style lang='scss'>
 </style>
