b771aa4634dbe7d19bfa8cf57136669a1545a858
lipeng <lipeng@daokoudai.com>
2019-11-27 14:35:55 +0800

    7.uni-app实战商城项目-uniRequest请求

--- pages/tabBar/home/index.vue
+++ pages/tabBar/home/index.vue
@@ -5,9 +5,32 @@
 
 <script>
 	import pageHeader from './pageHeader.vue'
+	import interfaces from '../../../utils/interfaces.js'
 	export default{
+		data(){
+			return {
+				swiperList:[], // 轮播图片
+				categoryList:[], // 分类
+				promotion:[], // 活动
+			}
+		},
 		components:{
 			pageHeader,
+		},
+		onLoad() {
+			this.initData()
+		},
+		methods:{
+			initData(){
+				uni.request({
+				    url: interfaces.getMallData,
+				    success: (res) => {
+				        this.swiperList = res.data.swiperList
+						this.categoryList = res.data.categoryList
+						this.promotion = res.data.promotion
+				    }
+				});
+			}
 		}
 	}
 </script>
