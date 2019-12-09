70e6148122a482b0b33bf552928221e56591dcef
lipeng <lipeng@daokoudai.com>
2019-12-09 18:32:58 +0800

    12.uni-app实战商城项目-渲染分类页面

--- utils/interfaces.js
+++ utils/interfaces.js
@@ -1,6 +1,8 @@
 const domain = 'https://uniapp-interface.herokuapp.com/';
 const interfaces = {
 	// 获取首页数据
-	getMallData: domain + 'api/profiles/mall_list'
+	getMallData: domain + 'api/profiles/mall_list',
+	// 获取分类数据
+	getCategory: domain + 'api/profiles/category'
 }
 module.exports = interfaces
